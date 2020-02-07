f96f0c07b5e9d47a71bb4a2c1a04c1b3a9e6fa4e
Kyle Bouwman <kybouw@users.noreply.github.com>
2020-02-06 19:46:24 -0800

    started working on install script
    Began work on an install script that will build the program and make an
    executable

---
+++ install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+################################################################################
+# Budg
+# install.sh
+# by Kyle Bouwman
+# subject to MIT License (see LICENSE)
+#
+# Install script for the Budg python program
+################################################################################
+
