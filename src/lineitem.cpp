030da5e42f03866fff940604c14c16e1017c2c53
Kyle Bouwman <kybouw@users.noreply.github.com>
2020-10-16 16:24:54 -0700

    change budget_value to double. Add getter/setters.
    I figured that since we are doing everything more high level just to
    finish it, we should just keep it as a double. We can always go back and
    upgrade/optimize as needed.

--- src/lineitem.cpp
+++ src/lineitem.cpp
@@ -3,10 +3,28 @@
 #include <string>
 #include "lineitem.hpp"
 
-LineItem::LineItem(int budget_value, std::string name) {
+LineItem::LineItem(double budget_value, std::string name) {
     this->budget_value = budget_value;
     this->name = name;
 }
 
 LineItem::~LineItem() {
 }
+
+double LineItem::get_budget_value() {
+    return this->budget_value;
+}
+
+void LineItem::set_budget_value(double a) {
+    this->budget_value = a;
+}
+
+std::string LineItem::get_name() {
+    return this->name;
+}
+
+void LineItem::set_name(std::string n) {
+    this->name = n;
+}
+
+
--- src/lineitem.hpp
+++ src/lineitem.hpp
@@ -1,15 +1,30 @@
+/* lineitem.hpp */
+
 #ifndef LINEITEM_HPP
 #define LINEITEM_HPP
 
 class LineItem {
     private:
         //stuff
-        int budget_value;
+
+        /*
+         * budget_value is a decimal value representing the percentage
+         * of the budget that will be allocated to this LineItem.
+         * 0 <= budget_value <= 1
+         */
+        double budget_value;
+
+        /* if you want to name your plan */
         std::string name;
+
     public:
         //more stuff
-        LineItem(int budget_value, std::string name);
+        LineItem(double budget_value, std::string name);
         ~LineItem();
+        double get_budget_value();
+        void set_budget_value(double a);
+        std::string get_name();
+        void set_name(std::string n);
 };
 
 #endif
