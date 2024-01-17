bdbe37c95dd11ea338eeaa41ffd326ad6ea6a3e3
Kyle Bouwman <kybouw@gmail.com>
2024-01-16 22:25:00 -0800

    add documentation to items in budg package

--- budg/root.go
+++ budg/root.go
@@ -1,3 +1,27 @@
+// budg
+// 
+// Budgeting income the easy way
+// 
+// by Kyle Bouwman
+// 
+// Copyright (C) 2022-2024 Kyle Bouwman
+// 
+// This file is part of budg.
+// 
+// budg is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// budg is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with budg.  If not, see <https://www.gnu.org/licenses/gpl.html>.
+
+// Tools for percentage-based income budgeting.
 package budg
 
 import (
@@ -10,8 +34,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
+/*
+ Representation of a plan.
+ The outer and inner string keys are group name and item name, respectively.
+ The float value pointed to by the keys is a percentage amount (0-100) that
+ represents how much of the total budget belongs to that item.
+*/
 type BudgPlan map[string]map[string]float64
 
+/*
+ Calculates the budget totals for each item and builds a string table
+ for output to the screen.
+*/
 func (plan BudgPlan) CalculateTable(amount float64) string {
 	var ret string = ""
 	for groupName, groupValue := range plan {
@@ -47,6 +81,7 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+// Cobra function. Executes the command.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
