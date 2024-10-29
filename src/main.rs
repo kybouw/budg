d18f19500b6ef961a726e2c648328e55da29d816
Kyle Bouwman <kyle@kybouw.com>
2024-10-28 21:00:19 -0700

    begin parsing plan from toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+
+use rust_decimal_macros::dec;
+use toml::Table;
+
+const MAX_WIDTH: i32 = 30;
+
+fn print_divider() {
+    for _ in 1..MAX_WIDTH {
+        print!("=")
+    }
+    println!("")
+
+}
 fn main() {
-    println!("Hello, world!");
+    let plan_file_contents = fs::read_to_string("static/default.toml").expect("failed to read static/default.toml");
+    let plan_table = plan_file_contents.parse::<Table>().expect("failed to parse table");
+
+    let budget_total = dec!(500);
+    
+    for (major_label, minor_table) in plan_table {
+        print_divider();
+        let major_budget_total = dec!(0);
+
+        // TODO unwrap Value enum into String (for 1-level plan) or Table (for 2-level plan)
+        // for (minor_label, minor_value) in minor_table {
+            
+        // }
+    }
 }
