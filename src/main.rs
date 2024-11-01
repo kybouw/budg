57584e2d7d95425826d5503883f5eefe74586818
Kyle Bouwman <kyle@kybouw.com>
2024-10-31 21:56:00 -0700

    finish building output table

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,75 @@
 use std::fs;
 
+use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
-use toml::Table;
+use toml::{Table, Value};
 
-const MAX_WIDTH: i32 = 30;
-
-fn print_divider() {
-    for _ in 1..MAX_WIDTH {
-        print!("=")
-    }
-    println!("")
-
+enum Divider {
+    Dash,
+    Equals,
 }
-fn main() {
-    let plan_file_contents = fs::read_to_string("static/default.toml").expect("failed to read static/default.toml");
-    let plan_table = plan_file_contents.parse::<Table>().expect("failed to parse table");
 
-    let budget_total = dec!(500);
-    
-    for (major_label, minor_table) in plan_table {
-        print_divider();
-        let major_budget_total = dec!(0);
-
-        // TODO unwrap Value enum into String (for 1-level plan) or Table (for 2-level plan)
-        // for (minor_label, minor_value) in minor_table {
-            
-        // }
+fn print_divider(divider: Divider) {
+    match divider {
+        Divider::Dash => {
+            println!("{:-^30}", "")
+        }
+        Divider::Equals => {
+            println!("{:=^30}", "")
+        }
     }
+}
+
+fn print_budget_line(total: Decimal, ratio: Decimal, name: String) -> Decimal {
+    let value = (total * ratio).trunc_with_scale(2);
+    println!("{:<20}${:>9}", name, value.to_string());
+    return value;
+}
+
+fn calculate_budget(total: Decimal) {
+    let plan_file_contents =
+        fs::read_to_string("static/default.toml").expect("failed to read static/default.toml");
+    let plan_table = plan_file_contents
+        .parse::<Table>()
+        .expect("failed to parse table");
+
+    for (major_label, major_value) in plan_table {
+        print_divider(Divider::Equals);
+
+        match major_value {
+            Value::Float(v) => {
+                let d = Decimal::from_str_exact(&v.to_string()).expect("failed to parse decimal");
+                print_budget_line(total, d / dec!(100), major_label);
+            }
+            Value::Integer(v) => {
+                let d = Decimal::from_str_exact(&v.to_string()).expect("failed to parse decimal");
+                print_budget_line(total, d / dec!(100), major_label);
+            }
+            Value::Table(t) => {
+                let mut major_budget_total = dec!(0);
+                for (minor_label, minor_value) in t {
+                    let r = match minor_value {
+                        Value::Float(v) => Decimal::from_str_exact(&v.to_string())
+                            .expect("failed to parse decimal"),
+                        Value::Integer(v) => Decimal::from_str_exact(&v.to_string())
+                            .expect("failed to parse decimal"),
+                        _ => panic!("not a number"),
+                    } / dec!(100);
+                    major_budget_total += print_budget_line(total, r, minor_label);
+                }
+                print_divider(Divider::Dash);
+                println!(
+                    "{:<20}${:>9}",
+                    major_label,
+                    major_budget_total.trunc_with_scale(2)
+                )
+            }
+            _ => panic!("not a number"),
+        }
+    }
+    print_divider(Divider::Equals);
+}
+
+fn main() {
+    calculate_budget(dec!(500));
 }
